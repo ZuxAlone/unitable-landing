cf2c1b4825403bb25973727767a43bd7a32c3d97
ZuxAlone <rmagdev20@gmail.com>
2022-11-19 01:01:02 -0500

    prom-video

--- src/components/Testimonies.jsx
+++ src/components/Testimonies.jsx
@@ -10,6 +10,10 @@ function Testimonies() {
             <TestimonyBox title="Testimonio 1" image={testImg1} text="Me gusta esta plataforma, puedo organizar mi horario de manera fácil y sencilla. Además, puedo conocer mas personas en los grupos de estudio."/>
             <TestimonyBox title="Testimonio 2" image={testImg2} text="Me encanta, es muy intuitiva y puedo encontrar estudiantes que tienen horarios flexibles que se adecuan a mi horas de enseñanza."/>
         </div>
+        <div className="prom-video">
+            <h1>Video Introductorio</h1>
+            <iframe width="560" height="315" src="https://www.youtube.com/embed/ccf12v83Cj8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
     </section>
   )
 }
